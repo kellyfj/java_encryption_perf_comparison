e2dadec6093ad6c14f320ad4d39b660d3930c135
Frank Kelly <frank.kelly@here.com>
2015-10-05 14:32:39 -0400

    Change order

--- src/com/kellyfj/Main.java
+++ src/com/kellyfj/Main.java
@@ -2,13 +2,26 @@ package com.kellyfj;
 
 import org.apache.commons.codec.binary.Base64;
 
+import java.security.SecureRandom;
+
 public class Main {
   private static final String TEST_STRING = "The quick brown fox jumped over the lazy dog";
   private static final int MAX_RUNS = 1000;
   private static final byte[] GCM_KEY_BYTES = Base64.decodeBase64("oa7vzk8r1fvEAXsUb2mlpW/+rS8KEfVlh6by+Sv+qb0=");
   private static final String SCBE_STAGING_ECB_KEY = "oa7vzk8r1fvEAXsUb2mlpW/+rS8KEfVlh6by+Sv+qb0=";
 
+  private static final SecureRandom random = new SecureRandom();
+  private static final byte[][] arrayOfIvs = new byte[MAX_RUNS][16];
+
   public static void main(String[] args) throws Exception {
+
+    //Initialize enough IVs
+    for(int i=0; i< MAX_RUNS; i++) {
+      arrayOfIvs[i] = getRandomIV();
+    }
+
+    doGCMEncryption();
+    System.out.println("==================");
 
     doECBEncryption();
     System.out.println("==================");
@@ -16,9 +29,16 @@ public class Main {
     doCBCEncryption();
     System.out.println("==================");
 
-    doGCMEncryption();
-    System.out.println("==================");
+
   }
+
+
+  private  static byte[] getRandomIV() {
+    byte[] iv = new byte[16];
+    random.nextBytes(iv);
+    return iv;
+  }
+
 
   private static void doGCMEncryption() throws Exception {
     IKeyFetcher kf = new KeyFetcher();
@@ -54,7 +74,7 @@ public class Main {
     long start = System.currentTimeMillis();
     for (int i = 0; i < 20; i++) {
       byte[] data = (TEST_STRING + i).getBytes();
-      encrypted = AES_CBC_CryptoService.encrypt(data, kf.getKey(), kf.getIV(), kf.getSalt());
+      encrypted = AES_CBC_CryptoService.encrypt(data, kf.getKey(), arrayOfIvs[i], kf.getSalt());
     }
     long end = System.currentTimeMillis();
     System.out.println("CBC --> " + new String(encrypted, "UTF8"));
@@ -64,7 +84,7 @@ public class Main {
     //CBC Decryption
     start = System.currentTimeMillis();
     for (int i = 0; i < 20; i++) {
-      byte[] decrypted = AES_CBC_CryptoService.decrypt(encrypted, kf.getKey(), kf.getIV(), kf.getSalt());
+      byte[] decrypted = AES_CBC_CryptoService.decrypt(encrypted, kf.getKey(), arrayOfIvs[i], kf.getSalt());
       backAgain = new String(decrypted, "UTF8");
     }
     end = System.currentTimeMillis();
